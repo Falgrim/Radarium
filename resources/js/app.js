a2181609ade8f12385fd6a890393e326744e5aee
Nikita Kurdin <shapeshifter2008@yandex.ru>
2025-01-04 17:34:37 +0300

    Вывод главной страницы

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import './bootstrap';
 
+import '../scss/app.scss'; // out SCSS file
+import * as bootstrap from 'bootstrap' // Bootstrap JS files
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,23 @@
+<x-global-layout>
+    <div class="my-5">
+        <div class="p-5 text-center bg-body-tertiary">
+            <div class="container py-5">
+                <h1 class="text-body-emphasis">Заголовок</h1>
+                <p class="col-lg-8 mx-auto lead">
+                   Какой-то текст
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="b-example-divider"></div>
+    <div class="my-5">
+        <div class="p-5 text-center bg-body-tertiary">
+            <div class="container py-5">
+                <h1 class="text-body-emphasis">Заголовок</h1>
+                <p class="col-lg-8 mx-auto lead">
+                    Какой-то текст
+                </p>
+            </div>
+        </div>
+    </div>
+</x-global-layout>
