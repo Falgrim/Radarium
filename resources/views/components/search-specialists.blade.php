d0f97558db3d43b4013b8a01728ff6c12048a689
Nikita Kurdin <shapeshifter2008@yandex.ru>
2025-02-10 10:45:30 +0300

    Скрытие формы поиска

--- resources/views/components/search-specialists.blade.php
+++ resources/views/components/search-specialists.blade.php
@@ -17,32 +17,36 @@
                         Настройки поиска:
                     </div>
                     <div class="col-4 text-end">
-                        Свернуть/Развернуть
+                        <a class="btn btn-light btn-sm" data-bs-toggle="collapse" href="#collapseSearchForm" role="button" aria-expanded="false" aria-controls="collapseSearchForm">
+                            Свернуть/Развернуть
+                        </a>
                     </div>
                 </div>
 
-                <form class="needs-validation {{ $formErrorClass }} mt-2" method="GET" action="{{ route('catalog.specialists') }}" novalidate="">
-                    <div class="row g-3">
-                        <div class="col-sm-6">
-                            <x-input-label for="key_word" class="form-label" :value="__('Ключевое слово')" />
-                            <input type="text" class="form-control" name="key_word" id="key_word" placeholder="" value="{{ old('key_word', $request->key_word) }}">
-                            <x-input-validate :messages="$errors->get('key_word')" />
+                <div class="collapse" id="collapseSearchForm">
+                    <form class="needs-validation {{ $formErrorClass }} mt-2" method="GET" action="{{ route('catalog.specialists') }}" novalidate="">
+                        <div class="row g-3">
+                            <div class="col-sm-6">
+                                <x-input-label for="key_word" class="form-label" :value="__('Поиск в сообщении')" />
+                                <input type="text" class="form-control" name="key_word" id="key_word" placeholder="" value="{{ old('key_word', $request->key_word) }}">
+                                <x-input-validate :messages="$errors->get('key_word')" />
+                            </div>
+
+                            <div class="col-md-6">
+                                <x-input-label for="speciality_id" class="form-label" :value="__('Специальность')" />
+                                <select class="form-select" name="speciality_id" id="speciality_id">
+                                    <option value="">Выберите...</option>
+                                    @foreach ($specialitiesList as $speciality)
+                                        <option value="{{ $speciality['id'] }}" {{ (collect(old('speciality_id', $request['speciality_id']))->contains($speciality['id'])) ? 'selected':'' }}>{{ $speciality['value'] }}</option>
+                                    @endforeach
+                                </select>
+                                <x-input-validate :messages="$errors->get('speciality_id')" />
+                            </div>
                         </div>
 
-                        <div class="col-md-6">
-                            <x-input-label for="speciality_id" class="form-label" :value="__('Специальность')" />
-                            <select class="form-select" name="speciality_id" id="speciality_id">
-                                <option value="">Выберите...</option>
-                                @foreach ($specialitiesList as $speciality)
-                                    <option value="{{ $speciality['id'] }}" {{ (collect(old('speciality_id', $request['speciality_id']))->contains($speciality['id'])) ? 'selected':'' }}>{{ $speciality['value'] }}</option>
-                                @endforeach
-                            </select>
-                            <x-input-validate :messages="$errors->get('speciality_id')" />
-                        </div>
-                    </div>
-
-                    <button class="w-10 btn btn-primary mt-3" type="submit">Поиск</button>
-                </form>
+                        <button class="w-10 btn btn-primary mt-3" type="submit">Поиск</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
