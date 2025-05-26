650f553f499c21f0c588b3c3928ec6acc3fa627c
Nikita Kurdin <shapeshifter2008@yandex.ru>
2025-05-26 20:34:57 +0300

    RAD-196 Счетчик на главной

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Enum\ApiPostAiStatusEnum;
 use App\Enum\PaymentTariffStatusEnum;
 use App\Models\ApiPostUser;
 use App\Models\PaymentTariff;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        $contactSum = ApiPostUser::count();
-        $contactTodaySum = ApiPostUser::where('created_at', '>=', Carbon::now()->startOfDay())->count();
+        $contactSum = ApiPostUser::whereHas('specialists', function (Builder $query) {
+            $query->where('status', '=', ApiPostAiStatusEnum::Active);
+        })->count();
+
+        $contactTodaySum = ApiPostUser::whereHas('specialists', function (Builder $query) {
+            $query->where('status', '=', ApiPostAiStatusEnum::Active);
+            $query->where('created_at', '>=', Carbon::now()->startOfDay());
+        })->count();
 
         $tariffs = PaymentTariff::where('status', PaymentTariffStatusEnum::Active)
             ->orderBy('period', 'ASC')
