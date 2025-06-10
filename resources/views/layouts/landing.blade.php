a6e9d55fa58534b3d420a60e967d575d89dc0f44
Nikita Kurdin <shapeshifter2008@yandex.ru>
2025-06-10 11:23:09 +0300

    RAD-245 Редизайн

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="{{ asset('v2/fonts/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('v2/fonts/fontawesome5-overrides.min.css') }}">
     <link rel="stylesheet" href="{{ asset('v2/css/Navbar-Centered-Links-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('v2/css/styles.css') }}?v=1.1">
+    <link rel="stylesheet" href="{{ asset('v2/css/styles.css') }}?v=1.2">
 </head>
 
 <body>
