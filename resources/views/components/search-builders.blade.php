19ffd59b3de68f6a8cb8e0c58fe1663905f6dcbb
Alexander Golovin <59964007+Falgrim@users.noreply.github.com>
2026-04-07 17:44:20 +0300

    UX/UI дизайн правка №1 -  преключение экранов строители и проектировщики табами

--- resources/views/components/search-builders.blade.php
+++ resources/views/components/search-builders.blade.php
@@ -12,15 +12,7 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <div class="row-flex">
-                <h5 class="tab-heading">Строительство</h5>
-                <button class="btn btn-link btn-trans btn-trash" type="reset" onclick="window.location.href='{{ route('catalog.builders') }}'">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-32 0 512 512" width="1em" height="1em" fill="currentColor">
-                        <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"></path>
-                    </svg>
-                    <span>Очистить поиск</span>
-                </button>
-            </div>
+            <x-catalog-search-tabs />
             <form class="needs-validation {{ $formErrorClass }}" id="search_form" method="GET" action="{{ route('catalog.builders') }}" novalidate="">
                 <div class="search-field">
                     <div class="search-info search-item">
--- resources/views/components/search-specialists.blade.php
+++ resources/views/components/search-specialists.blade.php
@@ -13,15 +13,7 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <div class="row-flex">
-                <h5 class="tab-heading">Проектирование</h5>
-                <button class="btn btn-link btn-trans btn-trash" type="reset" onclick="window.location.href='{{route('catalog.specialists')}}'">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-32 0 512 512" width="1em" height="1em" fill="currentColor">
-                        <path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0H284.2c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64S14.3 32 32 32h96l7.2-14.3zM32 128H416V448c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16V432c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"></path>
-                    </svg>
-                    <span>Очистить поиск</span>
-                </button>
-            </div>
+            <x-catalog-search-tabs />
             <form class="needs-validation {{ $formErrorClass }}" id="search_form" method="GET" action="{{ route('catalog.specialists') }}" novalidate="">
                 <div class="search-field">
                     <div class="search-info search-item">
@@ -72,7 +64,7 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em" fill="currentColor" hidden="">
                                         <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"></path>
                                     </svg>
-                                    <input class="form-control" type="text" name="key_word" id="key_word" {{ old('key_word', $request->key_word) }} placeholder="Введите слово и нажмите Enter" title="Введите слово и нажмите Enter">
+                                    <input class="form-control" type="text" name="key_word" id="key_word" value="{{ old('key_word', $request->key_word) }}" placeholder="Введите слово и нажмите Enter" title="Введите слово и нажмите Enter">
                                 </span>
                                 <ul class="list-group tags" id="buttonsContainer">
                                     @if (isset($request->key_word_tags))
